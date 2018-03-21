904e7468fd127d11b80e1ecf8b4413f8af446dc8
titopuertolara <andrespuertolara@gmail.com>
2018-03-21 10:29:39 -0500

    adding some comments

--- Codigos_practicas/Analog_value_Arduino_omega_Raspberry.py
+++ Codigos_practicas/Analog_value_Arduino_omega_Raspberry.py
@@ -4,6 +4,7 @@ import sys
 import serial
 import time
 #Cambiar API_KEY por la clave de la plataforma
+#Dependiendo de si es omega o Raspberry PI el valor ttyS1 puede cambiar a ttyACM0, consulte con su profesor
 puerto=serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1.0 )
 url="https://api.thingspeak.com/update?api_key=API_KEY"
 
